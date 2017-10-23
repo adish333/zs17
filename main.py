5d9f0d81f3d645ed47a62f26b052d3225b667f53
Adish Jain <jain.adish333@gmail.com>
2017-10-24 01:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from keras.layers import TimeDistributed
 import json
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Hide messy TensorFlow warnings
 warnings.filterwarnings("ignore")  # Hide messy Numpy warnings
-print("backup_file running")
 
 def doWork(train, user):
     SEQ_LENGTH = 5
@@ -46,7 +45,6 @@ def doWork(train, user):
             target_sequence[j][y_sequence_ix[j]] = 1.
         y[i] = target_sequence
 
-
     model = Sequential()
     model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
     for j in range(LAYER_NUM - 1):
@@ -56,7 +54,7 @@ def doWork(train, user):
     model.add(Activation('softmax'))
     model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
     model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, nb_epoch=nb_epoch)
-    model.save_weights('D:\zs17\model\\user_{}_checkpoint_{}_epoch_{}.hdf5'.format(i, HIDDEN_DIM, nb_epoch))
+    model.save_weights('user_{}_checkpoint_{}_epoch_{}.hdf5'.format(i, HIDDEN_DIM, nb_epoch))
     output = generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, chars)
     return output
 
@@ -74,11 +72,10 @@ def generate_text(model, length, VOCAB_SIZE, chars):
     return y_char
 
 
-train=pd.read_csv("D:\zs17\\data.csv")
+train=pd.read_csv("data.csv")
 train['event']= train['event'].astype("category")
-userdata=pd.read_csv("D:\zs17\\user.csv")
+userdata=pd.read_csv("user.csv")
 users = userdata['PID']
-users=users[100:] #change here a. user[:100] b. user[2500:]
 not_done=[]
 submission = pd.DataFrame()
 
@@ -89,27 +86,24 @@ for user in users:
     try:
         out = doWork(train, user)
         submission[user]=out
-        file='D:\zs17\output\\{}.json'.format(user)
+        file='{}.json'.format(user)
         with open(file, 'w') as outfile:
             json.dump(out, outfile)
     except:
         not_done.append(user)
         continue
     if(x==50):
-        f1='D:\zs17\\notdone{}.csv'.format(user)
-        f2='D:\zs17\\submission{}.csv'.format(user)
+        f1='notdone{}.csv'.format(user)
+        f2='submission{}.csv'.format(user)
         remaining = pd.DataFrame()
         remaining['notdone'] = not_done
         remaining.to_csv(f1)
         submission.to_csv(f2)
-        x=0
+        x=0     
         
-        
-        
-    
 remaining = pd.DataFrame()
 remaining['notdone'] = not_done
-remaining.to_csv('D:\zs17\\notdone.csv')
-submission.to_csv('D:\zs17\\submission.csv')
+remaining.to_csv('notdone.csv')
+submission.to_csv('submission.csv')
 tf = timer()
 print("time taken: ",tf - ti)
